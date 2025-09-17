d07644886ebf8f766974cf74b1900535ee74e61a
Juan Camilo Victoria <jcvictoriap@gmail.com>
2025-09-17 16:36:32 -0500

    Estructure project with MVC pattern.

---
+++ Todoey/DataModel/Item.swift
@@ -0,0 +1,12 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Juan Camilo Victoria Pacheco on 17/09/25.
+//  Copyright © 2025 App Brewery. All rights reserved.
+//
+
+class Item {
+  var title: String = ""
+  var done: Bool = false
+}
